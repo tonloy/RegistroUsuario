a425517b9f8f9cd591cb8c309479f6798e2a72f7
tonloy <manciatonloy@hotmail.com>
2018-09-20 21:30:30 -0600

    Agregar archivos de proyecto.

---
+++ RegistroUsuarios/RegistroUsuarios/Models/Usuario.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace RegistroUsuarios.Models
+{
+    public class Usuario
+    {
+        [PrimaryKey,AutoIncrement]
+        public int Id { get; set; }
+        public string N_usuario { get; set; }
+        public string Clave { get; set; }
+        public string Imagen { get; set; }
+
+        private UsuarioDBContext db;
+
+        public Usuario()
+        {
+
+        }
+
+        public Usuario(UsuarioDBContext BaseDatos)
+        {
+            this.db = BaseDatos;
+        }
+
+        //Método para guardar
+        public Task<bool> GuardarTablaAsincrona(Usuario tabla)
+        {
+            if (this.db.Conexion.InsertAsync(tabla).Result == 1)
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+        }
+
+        //Método para la ejecución de una Query
+        public Task<List<Usuario>> QueryAsincrona(string query)
+        {
+            return this.db.Conexion.QueryAsync<Usuario>(query);
+        }
+
+        //Metodo para eliminar
+        public Task<bool> EliminarTablaAsincrona(Usuario tabla)
+        {
+            if (this.db.Conexion.DeleteAsync(tabla).Result == 1)
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+        }
+
+        //metodo para actualizar datos
+        public Task<bool> ActualizarTablaAsincrona(Usuario tabla)
+        {
+            if (this.db.Conexion.UpdateAsync(tabla).Result == 1)
+            {
+                return Task.FromResult(true);
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
---
+++ RegistroUsuarios/RegistroUsuarios/Views/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RegistroUsuarios
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowPass(object sender, EventArgs args)
+        {
+            txtPassword.IsPassword = txtPassword.IsPassword ? false : true;
+            iconoOjo.Source = ImageSource.FromFile(txtPassword.IsPassword==true?"eye.png":"hide.png");
+        }
+    }
+}
